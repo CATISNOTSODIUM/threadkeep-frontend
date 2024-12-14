ec0548675171917cddce896281d2a67a743531ba
Kosolpattanadurong Thitiwat <thitiwatipst@icloud.com>
2024-12-14 13:34:51 +0700

    add creating thread api

--- src/api/threads.ts
+++ src/api/threads.ts
@@ -1,3 +1,4 @@
+import { User } from "../models";
 
 const HOST_API = "http://localhost:8000";
 
@@ -10,6 +11,29 @@ export const threadList = async () => {
         .then((data) => data.payload.data)
         .catch((e) => {throw e})
         
+        return response
+    } catch (error) {
+        console.error("Error in retrieving document list", error);
+        throw error;
+    }
+}
+
+export const createNewThread = async (user: User, title: string, content: string) => {
+    try {
+        const response = await fetch(`${HOST_API}/threads/create`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${user.id}`,
+            },
+            body: JSON.stringify({
+               title: title,
+               content: content,
+               user: user
+            }),
+        }).then((res) => res.json())
+        .then((data) => data.payload.data)
+        .catch((e) => {throw e})
         return response
     } catch (error) {
         console.error("Error in retrieving document list", error);
